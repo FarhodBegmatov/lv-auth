a71c6c7565a224caa1bfa64b1fa73817ae1693e2
Ismingiz <email@example.com>
2024-12-06 18:35:49 +0500

    added 3 functions to UserController and added routes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function create()
+    {
+        return view('user.create');
     
+    }
+
+    public function create()
+    {
+        dd($request->all());
+    
+    }
+
+    public function login()
+    {
+        return view('user.login');
+    
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('register', [UserController::class, 'create'])->name('register');
+Route::post('register', [UserController::class, 'store'])->name('user.store');
+Route::get('login', [UserController::class, 'login'])->name('login');
+
